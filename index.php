c70a68928cd1bd978fe540a4382c9cf85e9f7ac8
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-06-26 21:29:34 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>vercel-api-php</title>
+</head>
+<body>
+    <a href="api/">View api</a><br>
+    <a href="api/functions/">View functions</a><br>
+    <a href="test/">View test</a><br>
+    <a href="test/info/phpinfo.php">View phpinfo</a><br>
+</body>
+</html>';
+?>
